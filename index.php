594fe683f1f0015ecb41e61a993f2fbf16b5ebc8
xanfox <alexandro.mdc@gmail.com>
2022-06-21 18:04:52 -0300

    add font awesome

--- index.php
+++ index.php
@@ -6,16 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hello Bulma!</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
     <section class="section">
         <div class="container">
             <h1 class="title">
-                Hello World
+                Bulma SandBox
             </h1>
             <p class="subtitle">
-                My first website with <strong>Bulmar</strong>!
+                Everything you need to start with <strong>Bulma</strong>!
             </p>
         </div>
     </section>
